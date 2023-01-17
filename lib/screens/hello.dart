cadb234edbac96b8f6cfa547cd96471a9b8469e2
Gautam <gk9765551@gmail.com>
2023-01-17 19:45:43 +0530

    Onboarding screen completed

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -1,35 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-      body: SafeArea(
-          child: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 60,
-              height: double.infinity,
-              color: Colors.red,
-              child: Text("I am red"),
-            ),
-            Container(
-              width: 60,
-              color: Colors.blue,
-              height: double.infinity,
-              child: Text("I am blue"),
-            ),
-            Container(
-              width: 60,
-              color: Colors.green,
-              height: double.infinity,
-              child: Text("I am green"),
-            )
-          ],
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(child: Center(
+        child: Text("hello world"),
       )),
-    )),
-  );
+    ),
+  ));
 }
