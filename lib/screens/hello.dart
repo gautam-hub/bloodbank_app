1e7fc6b9babec161bfa857a6c9dd414c377c9264
Gautam <gk9765551@gmail.com>
2023-01-17 19:06:46 +0530

    container allignment using row

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
       body: SafeArea(
-        child: Center(
-        child: Container(
-          child: Image.asset("assets/bloodbank.png"),
+          child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 60,
+              height: double.infinity,
+              color: Colors.red,
+              child: Text("I am red"),
+            ),
+            Container(
+              width: 60,
+              color: Colors.blue,
+              height: double.infinity,
+              child: Text("I am blue"),
+            ),
+            Container(
+              width: 60,
+              color: Colors.green,
+              height: double.infinity,
+              child: Text("I am green"),
+            )
+          ],
         ),
-      ),
-    )
-    );
-  }
+      )),
+    )),
+  );
 }
