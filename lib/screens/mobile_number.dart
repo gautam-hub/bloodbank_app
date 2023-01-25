72e4235ff0fbc974d6bb5f67badf192ad2cbba2c
Gautam <gk9765551@gmail.com>
2023-01-25 19:02:44 +0530

    otp changes

--- lib/screens/mobile_number.dart
+++ lib/screens/mobile_number.dart
@@ -1,13 +1,20 @@
-import 'dart:ui';
+// ignore_for_file: avoid_unnecessary_containers, duplicate_ignore, sized_box_for_whitespace
+
+import 'dart:developer';
 
 import 'package:bloodbank_app/constants/colors.dart';
+import 'package:bloodbank_app/constants/routes.dart';
 import 'package:flutter/material.dart';
 
-class MobileNumber extends StatelessWidget {
-  const MobileNumber({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController _phoneNumberController = TextEditingController();
+    log("---------------------------------------------");
+    log("---------mobile_number.dart------------");
+    log("---------------------------------------------");
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.remove),
@@ -30,35 +37,75 @@ class MobileNumber extends StatelessWidget {
             // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               //the enter number field
+              // ignore: avoid_unnecessary_containers
               Container(
                 child: Column(
                   children: [
                     Container(
-                      child: Text(
+                      child: const Text(
                         "Enter your mobile number",
                       ),
                     ),
                     Container(
-                      width: 265,
+                      // width: 365,
+                      // 48 as padding on both sides and 265 to be width
+                      // so the percentage is ~73%
+                      width: MediaQuery.of(context).size.width * 0.73,
                       margin: EdgeInsets.only(
                         top: 15,
                       ),
-                      padding: EdgeInsets.fromLTRB(60, 14, 60, 17),
+                      padding:
+                          const EdgeInsets.only(left: 40, top: 14, bottom: 17),
                       decoration: BoxDecoration(
                           border: Border.all(
                             width: 0.8,
                           ),
-                          borderRadius: BorderRadius.all(
+                          borderRadius: const BorderRadius.all(
                             Radius.circular(
                               20,
                             ),
                           )),
-                      child: Text(
-                        "+91",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: MyColors.redPrimary,
-                        ),
+                      child: Row(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "+91",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: MyColors.redPrimary,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 18,
+                              ),
+                              SizedBox(
+                                width: 180,
+                                child: TextField(
+                                  // validator: (value) => ,
+                                  // onChanged: (value) =>
+                                  //     {_phoneNumberController.text = value},
+                                  controller: _phoneNumberController,
+
+                                  decoration: InputDecoration(
+                                    // prefix: Text(
+                                    //   '+91',
+                                    //   style: TextStyle(
+                                    //     fontSize: 20,
+                                    //     color: MyColors.redPrimary,
+                                    //   ),
+                                    // ),
+                                    border: InputBorder.none,
+                                    //
+                                    hintText: 'Enter you number here',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ],
@@ -66,42 +113,30 @@ class MobileNumber extends StatelessWidget {
               ),
 
               // add some spacing
-              SizedBox(
+              const SizedBox(
                 height: 67,
               ),
-              TextButton(
-                onPressed: () => {},
-                child: Text(
-                  "Login's",
-                ),
-              ),
-              // coming from global theme
-              ElevatedButton(
-                onPressed: () => {},
-                child: Text(
-                  "Login's",
-                ),
-              ),
+
               ElevatedButton(
                 style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Colors.yellow,
+                  fixedSize: MaterialStateProperty.all(
+                    Size(
+                      MediaQuery.of(context).size.width * 0.73,
+                      53,
+                    ),
                   ),
                 ),
-                onPressed: () => {},
-                child: Text(
-                  "Login's",
-                ),
+                onPressed: () => {
+                  if (_phoneNumberController.text.isNotEmpty)
+                    {
+                      print(
+                          "Your phone number is ${_phoneNumberController.text}"),
+                      Navigator.pushNamed(context, Routes.otpScreen)
+                    }
+                },
+                child: const Text("Login"),
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.greenAccent,
-                ),
-                onPressed: () => {},
-                child: Text(
-                  "Login's",
-                ),
-              ),
+
               // Container(
               //   height: 53,
               //   width: 263,
